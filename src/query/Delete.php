22b85b0f160f2f5e78b0f5875644c9628b4eb798
bauer01 <bronislav.sedlak@gmail.com>
2014-03-07 10:57:09 +0100

    queries: added delete query [closes #12]

--- src/query/Delete.php
+++ src/query/Delete.php
@@ -2,18 +2,52 @@
 
 namespace UniMapper\Query;
 
+use UniMapper\Query\FindAll;
+
 /**
  * Delete query
  */
 class Delete extends \UniMapper\Query implements \UniMapper\Query\IConditionable
 {
 
-    public $limit = 0;
-
-    public function limit($limit)
+    protected function onExecute()
     {
-        $this->limit = $limit;
-        return $this;
+        if (count($this->conditions) === 0) {
+            throw new QueryException("At least one condition must be set!");
+        }
+
+        if ($this->entityReflection->isHybrid()) {
+            throw new \Exception("Not yet implemented");
+        } else {
+            foreach ($this->entityReflection->getMappers() as $mapperName => $mapperReflection) {
+                return $this->mappers[$mapperName]->delete($this);
+            }
+        }
+    }
+
+    private function deleteHybrid()
+    {
+        // @todo primary property must be required
+        $primaryProperty = $this->entityReflection->getPrimaryProperty();
+        if ($primaryProperty === null) {
+            throw new QueryException("Entity does not have primary property!");
+        }
+
+        // Try to get appropriate records first
+        $query = new FindAll($this->entityReflection, $this->mappers);
+        $query->conditions = $this->conditions;
+        $entities = $query->execute();
+
+        if (!$entities) {
+            return false;
+        }
+
+        $this->conditions = array(array($primaryProperty->getName(), "IN", $entities->getKeys(), "AND"));
+        foreach ($this->entityReflection->getMappers() as $mapperName => $mapperReflection) {
+            $this->mappers[$mapperName]->delete($this);
+        }
+
+        return true;
     }
 
 }
