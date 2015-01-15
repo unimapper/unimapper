25ceb777953660b72ab1c33a65181aacf8ae421a
bauer01 <bronislav.sedlak@gmail.com>
2015-01-15 13:44:07 +0100

    exceptions: AdapterException require query data

--- src/exception/AdapterException.php
+++ src/exception/AdapterException.php
@@ -5,4 +5,17 @@ namespace UniMapper\Exception;
 class AdapterException extends \UniMapper\Exception
 {
 
+    private $query;
+
+    public function __construct($message, $query)
+    {
+        parent::__construct($message);
+        $this->query = $query;
+    }
+
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
 }
